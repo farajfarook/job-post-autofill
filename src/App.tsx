6e7903d31dec9696f8bfb780d7f1848651acde92
Faraj Farook <farajfarook@gmail.com>
2023-01-27 03:30:29 +1100

    component file name changes

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,12 @@ function App() {
             <div className="flex-1 bg-base-200"></div>
             <div className="w-[800px] bg-base-300 h-screen p-4 flex flex-col space-y-8">
                 {!token && <Login onLogin={setToken} />}
-                {token && <Header onLogout={() => setToken(undefined)} />}
-                {token && <JobForm token={token} onError={setError} />}
+                {token && (
+                    <>
+                        <Header onLogout={() => setToken(undefined)} />
+                        <JobForm token={token} onError={setError} />
+                    </>
+                )}
                 {error}
             </div>
             <div className="flex-1 bg-base-200"></div>
